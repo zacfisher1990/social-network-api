c236a73427b5a821018272feb211fd0c9c250771
unknown <zcfshr@gmail.com>
2022-11-26 10:34:54 -0700

    User model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,37 @@
-
-
 const UserSchema = new Schema(
     {
         username: {
             type: String,
             unique: true,
-            trim:
-             true,
-            required: "Username is Required",
-        }
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            Required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+const Users = model('Users', UserSchema);
+
+module.exports = Users;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+// require Thoughts and Users
+const Thoughts = require('./Thoughts');
+const Users = require('./Users');
+
+// Export models
+module.exports = { Users, Thoughts };
